0f312febd10985187f565e88c09a491105564bf6
matrixfire <48238435+matrixfire@users.noreply.github.com>
2024-03-17 19:35:11 +0800

    ok

--- handle_files_folders.py
+++ handle_files_folders.py
@@ -1,3 +1,12 @@
+'''
+About 
+READING AND WRITING FILES
+ORGANIZING FILES
+
+
+'''
+
+
 import os
 import zipfile
 import shutil
@@ -201,6 +210,38 @@ def pds(path, indent='', structure=''):
 # - `ZipInfo` object provides attributes like `file_size` and `compress_size`, representing original and compressed sizes.
 # - Calculate compression ratio with `original file size / compressed file size`.
 
+
+def backupToZip(folder):
+    # Backup the entire contents of "folder" into a zip file.
+
+    folder = os.path.abspath(folder) # Make sure folder is absolute
+
+    # Figure out the filename this code should used based on what
+    # files already exist.
+    number = 1
+    while True:
+        zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
+        if not os.path.exists(zipFilename):
+            break
+        number = number + 1
+
+    # Create the zip file.
+    print('Creating %s...' % (zipFilename))
+    backupZip = zipfile.ZipFile(zipFilename, 'w')
+
+    # Walk the entire folder tree and compress the files in each folder.
+    for foldername, subfolders, filenames in os.walk(folder):
+        print('Adding files in %s...' % (foldername))
+        # Add the current folder to the zip file.
+        backupZip.write(foldername)
+
+        # Add all the files in this folder to the zip file.
+        for filename in filenames:
+            if filename.startswith(os.path.basename(folder) + '_') and filename.endswith('.zip'):
+                continue # Don't backup the backup zip files.
+            backupZip.write(os.path.join(foldername, filename))
+    backupZip.close()
+    print('Done.')
 
 
 
