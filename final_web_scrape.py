2bbe4adb0d9f36d875655614f98371c9ba40547e
Bill <you@example.com>
2024-03-30 18:25:11 +0800

    k

--- final_web_scrape.py
+++ final_web_scrape.py
@@ -16,10 +16,20 @@ Attribute Selector ([a=b]): Selects all elements with an attribute a equal to b.
 Pseudoclass Selector (a:b): Selects all elements that are in the state :b of the element <a>.
 
 Pseudoelement Selector (a::b): Selects all ::b pseudoelements on element <a>.
+
+
+4,
+Sure, here's a summary:
+
+- `soup.select('input[name]')`: This command is used to find and select all `<input>` elements that have a `name` attribute, regardless of the value of the `name` attribute.
+
+- `soup.select('input[type="button"]')`: This one is used to find and select all `<input>` elements that specifically have a `type` attribute set to the value `"button"`.
+
 '''
 
 
 import requests
+from bs4 import BeautifulSoup
 
 def save_file_from_url(url, filename):
     try:
@@ -38,3 +48,27 @@ def save_file_from_url(url, filename):
 
 # Example usage:
 save_file_from_url('https://automatetheboringstuff.com/files/rj.txt', 'RomeoAndJuliet2.txt')
+
+
+
+
+def parse_html_and_select(selector, file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    
+    soup = BeautifulSoup(content, 'html.parser')
+    elems = soup.select(selector)
+    
+    elements_summary = []
+    for elem in elems:
+        elements_summary.append({
+            'text': elem.getText(),
+            'attributes': elem.attrs
+        })
+    
+    summary = {
+        'count': len(elems),
+        'elements': elements_summary
+    }
+    
+    return summary
