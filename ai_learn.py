144aec5946e378bab30d8efd014e6bd2a4ac10c7
matrixfire <48238435+matrixfire@users.noreply.github.com>
2024-05-19 21:52:49 +0800

    k

--- ai_learn.py
+++ ai_learn.py
@@ -5,16 +5,19 @@ a2 = np.array([1,2,3,4.0])
 b = np.array([[1,2,3,4],[5,6,7,8]])
 print(b.size, b.shape, b.dtype, b[1,2], sep=", ")
 # two's complement representation is widely used in computers for representing signed integers due to its simplicity and efficiency in arithmetic operations.
+c = np.zeros((3, 4), dtype='uint32') + 3
+d = np.ones((3, 4)) * 4
+e = np.arange(12).reshape((3, 4))
+
+f = c**2 + d**2 + e + np.random.randint(0, 5, (3, 4))
+
+np.save("random.npy", f)
+f = np.load("random.npy")
 
 
-a = np.zeros((3,4), dtype="uint32")
-a[1,2] = 66
+f = np.arange(24).reshape((4, 3, 2))
+f[2, ...] = [[99, 99], [99, 99], [99, 99]]
 
-b = 11*np.ones((3,1))
-
-
-a = np.arange(12).reshape((3,4))
-a[1], a[1, :]
 
 
 """
