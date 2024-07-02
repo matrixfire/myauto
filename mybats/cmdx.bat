a9181aa6e43001fdbd1e104dc3b14ab17d653d0f
matrixfire <matrixbox@qq.com>
2024-07-02 18:17:27 +0800

    July 02, 2024, 06:17 PM

---
+++ mybats/cmdx.bat
@@ -0,0 +1,2 @@
+@echo off
+wt -d "C:\Users\Administrator\Desktop\work2024\myauto" ; new-tab -d "C:\Users\Administrator\Desktop\work2024\reobrix_site" cmd /k ".\my_env2\Scripts\activate && cd .\reobrix2"
