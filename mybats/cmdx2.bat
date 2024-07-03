ef4ffc53e0c539031bf509c341a406874afa254f
Your Name <you@example.com>
2024-07-03 22:26:54 +0800

    July 03, 2024, 10:26 PM

--- mybats/cmdx2.bat
+++ mybats/cmdx2.bat
@@ -1,2 +1,4 @@
 @echo off
-wt -d "C:\Users\34950\Desktop\full_time_work\myauto" ; new-tab -d "C:\Users\34950\Desktop\work" cmd /k ".\my_env\Scripts\activate && cd .\mysite2024"
+wt -d "C:\Users\34950\Desktop\full_time_work\myauto" ; new-tab -d "C:\Users\34950\Desktop\work" cmd /k ".\my_env\Scripts\activate && cd .\mysite2024" ; new-tab -d "C:\Users\34950\Desktop\work" cmd /k ".\my_env\Scripts\activate && cd .\mysite2024" ; new-tab -d "C:\Users\34950\Desktop\work" cmd /k ".\my_env\Scripts\activate && cd .\mysite2024"
+
+
