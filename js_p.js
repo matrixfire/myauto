25c9b3cfe80599861b48189622728475aac89209
matrixfire <48238435+matrixfire@users.noreply.github.com>
2024-07-22 21:15:34 +0800

    July 22, 2024, 09:15 PM

--- js_p.js
+++ js_p.js
@@ -9,8 +9,18 @@ array.forEach(function(element, index, array) {
     // Code to execute for each element
 });
 
+document.querySelector('h1')
 
 
+const li = document.createElement('li');
+document.querySelector('#tasks').append(li);
+
+
+setInterval(count, 1000);
+
+localStorage.getItem(key)
+localStorage.setItem(key, value)
+ 
 // 2, some basics
 
 let counter = 0;
@@ -24,6 +34,7 @@ function count() {
 }
 
 
+
 document.querySelector('form').onsubmit = function() {
     const button = document.querySelector('button');
     const head = document.querySelector('#hello');
@@ -34,12 +45,52 @@ document.querySelector('form').onsubmit = function() {
 };
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('select').onchange = function() {
+        document.querySelector('#hello').style.color = this.value;
+    }
+});
+
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('select').onchange = (event) => {
         document.querySelector('#hello').style.color = event.target.value;
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').onsubmit = function() {
+
+        // Send a GET request to the URL
+        fetch('https://api.exchangeratesapi.io/latest?base=USD')
+        // Put response into json form
+        .then(response => response.json())
+        .then(data => {
+            // Get currency from user input and convert to upper case
+            const currency = document.querySelector('#currency').value.toUpperCase();
+
+            // Get rate from data
+            const rate = data.rates[currency];
+
+            // Check if currency is valid:
+            if (rate !== undefined) {
+                // Display exchange on the screen
+                document.querySelector('#result').innerHTML = `1 USD is equal to ${rate.toFixed(3)} ${currency}.`;
+            }
+            else {
+                // Display error on the screen
+                document.querySelector('#result').innerHTML = 'Invalid Currency.';
+            }
+        })
+        // Catch any errors and log them to the console
+        .catch(error => {
+            console.log('Error:', error);
+        });
+        // Prevent default submission
+        return false;
+    }
+});
+
 
 // 3, events
 
@@ -50,3 +101,10 @@ onkeydown
 onload
 onblur
 onchange 
+
+
+// 4, properties and methods
+
+.value
+.value.length
+.innerHTML
